fc97d53b03a5789b3776b436a181e82680d4d1f5
mapper895 <mapper.895.mp@gmail.com>
2021-01-24 18:23:59 -0600

    6. Sign Up Section

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { Header, Title, HeaderContainer } from "../elements/Header";
+import { Boton } from "../elements/Boton";
+import { Form, Input, BotonContainer } from "../elements/FormElements";
+import { ReactComponent as SvgLogin } from "../img/registro.svg";
+import styled from "styled-components";
 
 const SignUp = () => {
-  return <h1>Registro Usuarios</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Crear cuenta</title>
+      </Helmet>
+
+      <Header>
+        <HeaderContainer>
+          <Title>Crear Cuenta</Title>
+          <div>
+            <Boton to="/iniciar-sesion">Iniciar Sesion</Boton>
+          </div>
+        </HeaderContainer>
+      </Header>
+
+      <Form>
+        <Svg />
+        <Input type="email" name="email" placeholder="Correo Electronico" />
+        <Input type="password" name="password" placeholder="Contraseña" />
+        <Input
+          type="password"
+          name="password2"
+          placeholder="Repetir la contraseña"
+        />
+      </Form>
+      <BotonContainer>
+        <Boton as="button" primary type="submit">
+          Crear Cuenta
+        </Boton>
+      </BotonContainer>
+    </>
+  );
 };
+
+const Svg = styled(SvgLogin)`
+  width: 100%;
+  max-height: 6.25rem; /* 100px */
+  margin-bottom: 1.25rem; /* 20px */
+`;
 
 export default SignUp;
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  background: "#F9F9F9",
+  primaryColor: "#5B69E2",
+  secundaryColor: "#000",
+  green: "#43A854",
+  red: "#E34747",
+  lightGrey: "#E8EFF1",
+  lightGrey2: "#CBDDE2",
+  lightBlue: "#8792F1",
+};
