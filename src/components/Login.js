a7ba34ba0aad651d43a81975c5d876d74b6659f5
mapper895 <mapper.895.mp@gmail.com>
2021-01-26 17:49:44 -0600

    12. Login Logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { Header, Title, HeaderContainer } from "../elements/Header";
 import { Boton } from "../elements/Boton";
 import { Form, Input, BotonContainer } from "../elements/FormElements";
 import { ReactComponent as SvgLogin } from "../img/login.svg";
 import styled from "styled-components";
+import { auth } from "../firebase/firebaseConfig";
+import Alert from "../elements/Alert";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertState, setAlertState] = useState(false);
+  const [alert, setAlert] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setAlertState(false);
+    setAlert({});
+
+    // We check on the client side if the email is valid.
+    const regularExpression = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+    if (!regularExpression.test(email)) {
+      setAlertState(true);
+      setAlert({
+        type: "error",
+        message: "Por favor ingrese un correo válido",
+      });
+      return;
+    }
+    if (email === "" || password === "") {
+      setAlertState(true);
+      setAlert({
+        type: "error",
+        message: "Por favor llene todos los datos",
+      });
+      return;
+    }
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      history.push("/");
+    } catch (e) {
+      setAlertState(true);
+
+      let message;
+      switch (e.code) {
+        case "auth/wrong-password":
+          message = "La contraseña no es correcta.";
+          break;
+        case "auth/user-not-found":
+          message = "No se encontró ninguna cuenta con ese correo.";
+          break;
+        default:
+          message = "Hubo un error al intentar iniciar sesión.";
+          break;
+      }
+      setAlert({ type: "error", message: message });
+    }
+  };
   return (
     <>
       <Helmet>
@@ -22,16 +84,34 @@ const Login = () => {
         </HeaderContainer>
       </Header>
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Svg />
-        <Input type="email" name="email" placeholder="Correo Electronico" />
-        <Input type="password" name="password" placeholder="Contraseña" />
+        <Input
+          type="email"
+          name="email"
+          placeholder="Correo Electronico"
+          value={email}
+          onChange={handleChange}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={handleChange}
+        />
+        <BotonContainer>
+          <Boton as="button" primary type="submit">
+            Iniciar Sesion
+          </Boton>
+        </BotonContainer>
       </Form>
-      <BotonContainer>
-        <Boton as="button" primary type="submit">
-          Iniciar Sesion
-        </Boton>
-      </BotonContainer>
+      <Alert
+        type={alert.type}
+        message={alert.message}
+        alertState={alertState}
+        setAlertState={setAlertState}
+      />
     </>
   );
 };
