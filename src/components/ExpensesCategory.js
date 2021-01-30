09e6d172e81300b5124a998fdff29daddcca9a9d
mapper895 <mapper.895.mp@gmail.com>
2021-01-29 20:22:05 -0600

    33. Category Month Expenses Structure

--- src/components/ExpensesCategory.js
+++ src/components/ExpensesCategory.js
@@ -3,8 +3,19 @@ import { Header, Title } from "../elements/Header";
 import { Helmet } from "react-helmet";
 import ReturnBtn from "../elements/ReturnBtn";
 import TotalExpensesBar from "./TotalExpensesBar";
+import useGetCategoryMonthExpenses from "../hooks/useGetCategoryMonthExpenses";
+import {
+  CategoryList,
+  CategoryListElements,
+  Category,
+  Value,
+} from "../elements/ListElements";
+import CategoryIcon from "../elements/CategoryIcon";
+import ConvertToCurrency from "../functions/ConvertToCurrency";
 
 const CategoryExpenses = () => {
+  const categoryExpenses = useGetCategoryMonthExpenses();
+
   return (
     <>
       <Helmet>
@@ -15,6 +26,18 @@ const CategoryExpenses = () => {
         <ReturnBtn />
         <Title>Gastos por Categoría</Title>
       </Header>
+
+      <CategoryList>
+        {categoryExpenses.map((element, index) => (
+          <CategoryListElements key={index}>
+            <Category>
+              <CategoryIcon id={element.categoria} /> {element.categoria}
+            </Category>
+            <Value>{ConvertToCurrency(element.cantidad)}</Value>
+          </CategoryListElements>
+        ))}
+      </CategoryList>
+
       <TotalExpensesBar />
     </>
   );
